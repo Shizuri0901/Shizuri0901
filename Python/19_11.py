4ef38bcdfda853fd9ee2d8c31dbccb47b6377f14
HuyVu0901 <candycorman@gmail.com>
2022-11-30 10:03:59 +0700

    30/11

--- Python/19_11.py
+++ Python/19_11.py
@@ -1,9 +1,9 @@
-a = float(input())
-if a >= 8:
-    print("Gioi")
-elif 8<a<=6.5:
-    print("Kha")
-elif 6.5<a<=5:
-    print("Trung binh")
-else:
-    print("Yeu kem")
+# a = float(input())
+# if a >= 8:
+#     print("Gioi")
+# elif 8<a<=6.5:
+#     print("Kha")
+# elif 6.5<a<=5:
+#     print("Trung binh")
+# else:
+#     print("Yeu kem")
